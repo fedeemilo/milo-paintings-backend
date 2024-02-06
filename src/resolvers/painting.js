91df9a793a26ee1f78f49691c48bf0ca2ac00142
Federico Milone <federico.milone@claro.com.ar>
2024-02-06 18:47:41 -0300

    Add error handling on controller

--- src/resolvers/painting.js
+++ src/resolvers/painting.js
@@ -1,55 +1,55 @@
-const cloudinary = require("cloudinary").v2;
-const upload = require("../config/multer");
-const { Painting } = require("../models/painting");
+const cloudinary = require('cloudinary').v2
+const upload = require('../config/multer')
+const { Painting } = require('../models/painting')
 
 const getPaintings = async (req, res) => {
-    const paintings = await Painting.find();
-    res.json(paintings);
-};
+    const paintings = await Painting.find()
+    res.json(paintings)
+}
 
 const getPaintingById = async (req, res) => {
-    const painting = await Painting.findById(req.params.id);
+    const painting = await Painting.findById(req.params.id)
 
-    if (!painting) return res.status(404).send("Painting not found");
+    if (!painting) return res.status(404).send('Painting not found')
 
-    res.json(painting);
-};
+    res.json(painting)
+}
 
 const createPainting = async (req, res) => {
     upload(req, res, async err => {
         if (err) {
-            // manejar errores
+            throw new Error(err)
         } else {
             try {
-                const result = await cloudinary.uploader.upload(req.file.path);
+                const result = await cloudinary.uploader.upload(req.file.path)
                 const newPainting = new Painting({
                     src: result.secure_url,
                     title: req.body.title,
                     paintingType: req.body.paintingType,
                     price: req.body.price
-                });
+                })
 
-                const savedPainting = await newPainting.save();
-                res.status(201).json(savedPainting);
+                const savedPainting = await newPainting.save()
+                res.status(201).json(savedPainting)
             } catch (err) {
-                // manejar errores
+                throw new Error(err)
             }
         }
-    });
-};
+    })
+}
 
 const updatePainting = async (req, res) => {
     upload(req, res, async err => {
         if (err) {
-            // manejar errores
+            throw new Error(err)
         } else {
             try {
-                let painting = await Painting.findById(req.params.id);
+                let painting = await Painting.findById(req.params.id)
 
                 if (!painting) {
                     return res
                         .status(404)
-                        .json({ msg: "Pintura no encontrada" });
+                        .json({ msg: 'Pintura no encontrada' })
                 }
 
                 let paintingData = {
@@ -57,36 +57,36 @@ const updatePainting = async (req, res) => {
                     paintingType:
                         req.body.paintingType || painting.paintingType,
                     price: req.body.price || painting.price
-                };
+                }
 
                 if (req.file) {
                     const result = await cloudinary.uploader.upload(
                         req.file.path
-                    );
-                    paintingData.src = result.secure_url;
+                    )
+                    paintingData.src = result.secure_url
                 }
 
                 painting = await Painting.findByIdAndUpdate(
                     req.params.id,
                     paintingData,
                     { new: true }
-                );
+                )
 
-                res.json(painting);
+                res.json(painting)
             } catch (err) {
-                // manejar errores
+                throw new Error(err)
             }
         }
-    });
-};
+    })
+}
 
 const DeletePainting = async (req, res) => {
-    const painting = await Painting.findByIdAndRemove(req.params.id);
+    const painting = await Painting.findByIdAndRemove(req.params.id)
 
-    if (!painting) return res.status(404).send("Painting not found");
+    if (!painting) return res.status(404).send('Painting not found')
 
-    res.json(painting);
-};
+    res.json(painting)
+}
 
 module.exports = {
     createPainting,
@@ -94,4 +94,4 @@ module.exports = {
     getPaintingById,
     updatePainting,
     DeletePainting
-};
+}
